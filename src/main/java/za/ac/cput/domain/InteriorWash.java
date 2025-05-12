e3bcebf2ab6e5f2396ad5ce77c82b0f896c99eba
viwej <viwejack427@gmail.com>
2025-05-13 01:04:06 +0200

    Creation of Pojos (didn't write code inside all of them) will do that when I wake up

---
+++ src/main/java/za/ac/cput/domain/InteriorWash.java
@@ -0,0 +1,42 @@
+package za.ac.cput.domain;
+
+public class InteriorWash extends Service{
+    private boolean engine_cleaning_included;
+
+    public boolean getIsEngineCleaningIncluded() {
+        return engine_cleaning_included;
+    }
+
+    InteriorWash(Builder builder){
+        this.engine_cleaning_included = builder.engine_cleaning_included;
+    }
+
+    @Override
+    public String toString() {
+        return "InteriorWash{" +
+                "engine_cleaning_included=" + engine_cleaning_included +
+                '}';
+    }
+    public static class Builder extends Service{
+        private boolean engine_cleaning_included;
+
+      /*  public Builder setServiceID(int serviceID) {
+            this.s = engine_cleaning_included;
+            return this;
+        }
+        */
+
+        public Builder setEngine_cleaning_included(boolean engine_cleaning_included) {
+            this.engine_cleaning_included = engine_cleaning_included;
+            return this;
+        }
+        Builder copy(InteriorWash interiorWash){
+            this.engine_cleaning_included = interiorWash.engine_cleaning_included;
+            return this;
+        }
+
+        InteriorWash build(){
+            return new InteriorWash(this);
+        }
+    }
+}
